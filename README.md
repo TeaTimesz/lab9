5bf9dfffb4d3f67941590bcbb4ea0d8ad379318c
TeaTimesz <rawiphatmak@gmail.com>
2024-12-13 15:56:07 +0700

    completed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:รวิภาส มากเพ็ง
+# STUDENT ID:670612128
--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan , interest , paid;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	cin >> paid;
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +17,22 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
-	return 0;
+	cout << fixed << setprecision(2);
+
+	for(int i = 1;i > 0;i++){
+			cout << setw(13) << left << i; 
+			cout << setw(13) << left << loan;
+			cout << setw(13) << left << loan * (interest / 100);
+			cout << setw(13) << left << loan + (loan * (interest / 100));
+			if((loan + (loan * (interest / 100))) - paid <= 0){
+				cout << setw(13) << left << loan + (loan * (interest / 100));		
+				cout << setw(13) << left << 0.00;
+				cout << "\n";
+				break;
+			}
+			cout << setw(13) << left << paid;		
+			cout << setw(13) << left << (loan + (loan * (interest / 100))) - paid;
+			cout << "\n";	
+			loan = (loan + (loan * (interest / 100))) - paid;
+	}
 }
